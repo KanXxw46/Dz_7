5fcb3feae19c304025c02c45da236e9c6816e8c4
KanXxw46 <1kan_andreikkkk@mail.ru>
2020-06-23 11:52:06 +0600

    HomeWork

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bankomat
+{
+    public class Account
+    {
+        stringfixed password[5];
+        char user_account[13];
+        long money;
+        void Open_account();
+        void Show_info();
+        long &Work_with_money(int m);
+        bool Enter_to_your_account();
+    }
+}
